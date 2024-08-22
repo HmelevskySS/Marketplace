7d80263ebd2a084aa60008b8081a22a1f1296f83
HmelevskySS <hmelevsky.ss@gmail.com>
2024-08-22 18:12:45 +0200

    Updated folders structure

--- UI_tests/playwright.config.ts
+++ UI_tests/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    video: "on",
+    video: "off",
   },
 
   /* Configure projects for major browsers */
